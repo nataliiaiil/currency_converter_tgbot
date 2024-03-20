c01642c732e1d8d3f180f11296c0b96eab1fcc4f
NataliiaZinchenko <nataellie@gmail.com>
2024-03-20 14:09:57 +0000

    added callback(call) function

--- main.py
+++ main.py
@@ -35,15 +35,29 @@ def amount(message):
 
     if amount > 0:
         markup = types.InlineKeyboardMarkup(row_width=2)
-        btn_usd_eur = types.InlineKeyboardButton('USD -> EUR', callback_data='usd/eur')
-        btn_eur_usd = types.InlineKeyboardButton('EUR -> USD', callback_data='eur/usd')
-        btn_uah_eur = types.InlineKeyboardButton('UAH -> EUR', callback_data='uah/eur')
-        btn_uah_usd = types.InlineKeyboardButton('UAH -> USD', callback_data='uah/usd')
+        btn_usd_eur = types.InlineKeyboardButton('USD / EUR', callback_data='usd/eur')
+        btn_eur_usd = types.InlineKeyboardButton('EUR / USD', callback_data='eur/usd')
+        btn_uah_eur = types.InlineKeyboardButton('UAH / EUR', callback_data='uah/eur')
+        btn_uah_usd = types.InlineKeyboardButton('UAH / USD', callback_data='uah/usd')
         markup.add(btn_usd_eur, btn_eur_usd, btn_uah_eur, btn_uah_usd)
         bot.send_message(message.chat.id, 'Choose currency pair', reply_markup=markup)
     else:
         bot.send_message(message.chat.id, 'Incorrect amount: it should be more than 0. Try again.')
         bot.register_next_step_handler(message, amount)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    '''
+    Function that works with callback data from buttons, described in amount(message) function
+    and actually converts entered amount
+    '''
+    values = call.data.upper().split('/')
+    res = currency.convert(amount, values[0], values[1])
+    bot.send_message(call.message.chat.id, f'Today, entered {amount} {values[0]} makes {res:.2f} {values[1]}')
+    bot.send_message(call.message.chat.id, 'If you want to convert another value, enter it here')
+    bot.register_next_step_handler(call.message, amount)
+
 
 
 bot.infinity_polling()
